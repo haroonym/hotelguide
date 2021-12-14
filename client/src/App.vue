cceb45a1129d75b9db6674d72f1325c3a0cd7ffe
haroonym <71009111+haroonym@users.noreply.github.com>
2021-12-14 22:33:35 +0100

    Änderungen vorgenommen

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,39 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="success" dark>
-      <div class="d-flex align-center">
-        <v-img src="../public/hotel.png" width="50px" class="mr-3"></v-img>
-        <h2 class="white--text">Hotelguide</h2>
-      </div>
-      <v-spacer></v-spacer>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-    </v-app-bar>
-
-    <v-navigation-drawer v-model="drawer" app right>
-      <v-list nav>
-        <v-list-item-group  active-class="green darken-1 white--text">
-          <v-list-item link :to="'/'">
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
-          <v-list-item link :to="'/admin'">
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Admin</v-list-item-title>
-          </v-list-item>
-          <v-list-item link :to="`/addhotel`">
-            <v-list-item-icon>
-              <v-icon>mdi-plus</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Hotel hinzufügen</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-
+    <LogoBar />
     <v-main hide-overlay>
       <router-view :hotels="hotels" @deleteHotel="deleteHotel" @refreshHotel="getHotels"></router-view
     ></v-main>
@@ -41,14 +8,17 @@
 </template>
 
 <script>
+import LogoBar from '@/components/LogoBar.vue';
 import axios from 'axios';
 export default {
   name: 'App',
   data() {
     return {
       hotels: [],
-      drawer: false,
     };
+  },
+  components: {
+    LogoBar,
   },
   created() {
     this.getHotels();
@@ -74,7 +44,6 @@ export default {
         this.books = data;
         this.getHotels();
       } catch (error) {
-    
         console.error(error);
       }
     },
---
+++ client/src/components/LogoBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <v-app-bar app color="success" dark>
+      <div class="d-flex align-center">
+        <v-img src="../../public/hotel.png" width="50px" class="mr-3"></v-img>
+        <h2 class="white--text">Hotelguide</h2>
+      </div>
+      <v-spacer></v-spacer>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app right>
+      <v-list nav>
+        <v-list-item-group active-class="green darken-1 white--text">
+          <v-list-item link :to="'/'">
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+          <v-list-item link :to="'/admin'">
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Admin</v-list-item-title>
+          </v-list-item>
+          <v-list-item link :to="`/addhotel`">
+            <v-list-item-icon>
+              <v-icon>mdi-plus</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Hotel hinzufügen</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
